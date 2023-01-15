66145a19a153deb577e68b9b0500cfa60e16fdc5
(es)김용민 <es-kbsys29@hybecorp.com>
2023-01-15 20:34:16 +0900

    2023 기념 데이터 갱신

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import styles from "./header.module.scss";
-import Button from "Components/Button/Button";
+import React, { useState } from 'react';
+import styles from './header.module.scss';
+import Button from 'Components/Button/Button';
 
 const Header = props => {
   const { history } = props;
@@ -10,8 +10,8 @@ const Header = props => {
   return (
     <>
       <div
-        className={`${styles["open-button"]} ${
-          isOpen ? styles["open-button--open"] : ""
+        className={`${styles['open-button']} ${
+          isOpen ? styles['open-button--open'] : ''
         }`}
         onClick={() => setIsOpen(!isOpen)}
       >
@@ -21,45 +21,45 @@ const Header = props => {
       </div>
 
       <div
-        style={isOpen ? { transform: "translate3d(0, 0, 0)" } : {}}
-        className={styles["header"]}
+        style={isOpen ? { transform: 'translate3d(0, 0, 0)' } : {}}
+        className={styles['header']}
       >
-        <div className={styles["header__profile"]}>
+        <div className={styles['header__profile']}>
           <img
-            src="https://avatars.githubusercontent.com/u/56548968?s=400&u=95d5a439e056ea9bc91e0ed20296481da592cb45&v=4"
-            alt="profile_image"
+            src='https://avatars.githubusercontent.com/u/56548968?s=400&u=95d5a439e056ea9bc91e0ed20296481da592cb45&v=4'
+            alt='profile_image'
           ></img>
         </div>
-        <div className={styles["header__title"]}>
-          <div className={styles["main-title"]}>김용민</div>
-          <div className={styles["sub-title"]}>Web Developer</div>
+        <div className={styles['header__title']}>
+          <div className={styles['main-title']}>김용민</div>
+          <div className={styles['sub-title']}>Web Developer</div>
         </div>
-        <div className={styles["header__social"]}>
-          <span className={styles["social-link"]}>
+        <div className={styles['header__social']}>
+          <span className={styles['social-link']}>
             <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://github.com/hjk70288"
+              target='_blank'
+              rel='noreferrer'
+              href='https://github.com/hjk70288'
             >
-              <i className="fab fa-github"></i>
+              <i className='fab fa-github'></i>
             </a>
           </span>
-          <span className={styles["social-link"]}>
+          <span className={styles['social-link']}>
             <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://www.instagram.com/kym.dev"
+              target='_blank'
+              rel='noreferrer'
+              href='https://www.instagram.com/_.kym.dev'
             >
-              <i className="fab fa-instagram"></i>
+              <i className='fab fa-instagram'></i>
             </a>
           </span>
-          <span className={styles["social-link"]}>
+          <span className={styles['social-link']}>
             <a
-              target="_blank"
-              rel="noreferrer"
-              href="mailto:kimyongmin1228@gmail.com"
+              target='_blank'
+              rel='noreferrer'
+              href='mailto:kimyongmin1228@gmail.com'
             >
-              <i className="far fa-envelope"></i>
+              <i className='far fa-envelope'></i>
             </a>
           </span>
         </div>
@@ -67,20 +67,20 @@ const Header = props => {
           onClick={() => {
             setIsOpen(false);
           }}
-          className={styles["header__links"]}
+          className={styles['header__links']}
         >
-          <Button selected={pathname === "/"} link="/">
+          <Button selected={pathname === '/'} link='/'>
             Home
           </Button>
-          <Button selected={pathname === "/me"} link="/me">
+          <Button selected={pathname === '/me'} link='/me'>
             About Me
           </Button>
-          <Button selected={pathname === "/project"} link="/project">
+          <Button selected={pathname === '/project'} link='/project'>
             Project
           </Button>
         </div>
 
-        <div className={styles["header__copyright"]}>
+        <div className={styles['header__copyright']}>
           © 2022. (KYM) all rights reserved.
         </div>
       </div>
--- src/Pages/Me/Me.jsx
+++ src/Pages/Me/Me.jsx
@@ -1,62 +1,90 @@
-import React from "react";
-import styles from "./me.module.scss";
-import useScrollFadeIn from "Hooks/useScrollFadeIn";
-import { Container, Row, Col } from "react-grid-system";
-import Timeline from "Components/Timeline/Timeline";
-import Progress from "Components/Progress/Progress";
+import React from 'react';
+import styles from './me.module.scss';
+import useScrollFadeIn from 'Hooks/useScrollFadeIn';
+import { Container, Row, Col } from 'react-grid-system';
+import Timeline from 'Components/Timeline/Timeline';
+import Progress from 'Components/Progress/Progress';
 
 const Me = () => {
   return (
-    <div {...useScrollFadeIn(2, 0.7, 0)} className={styles["me"]}>
-      <div className={styles["me__content"]}>
-        <div className={styles["me__title"]}>
+    <div {...useScrollFadeIn(2, 0.7, 0)} className={styles['me']}>
+      <div className={styles['me__content']}>
+        <div className={styles['me__title']}>
           About <span>Me</span>
         </div>
         <Container fluid>
           <Row>
             <Col sm={7}>
-              <div className={styles["me__introduce"]}>
+              <div className={styles['me__introduce']}>
                 안녕하세요 개발자 김용민입니다!
                 <br />
-                저는 현재 웹 프론트 / 백엔드 개발을 하고있으며, 주로 React와
-                Node.js 기반의 프로젝트를 다루고 있습니다.
+                저는 현재 프리랜서로 활동하고 있으며, 웹 개발을 합니다.
                 <br />
-                이곳은 제 소개와 주로 사용하는 기술 스택, 경험 등의 내용을
-                기술해놓은 공간입니다. 😼
+                프론트엔드 React, 백엔드 Node.js / Spring Boot를 주로 사용하고
+                있습니다. 😼
               </div>
             </Col>
             <Col sm={5}>
-              <div className={styles["me__contact"]}>
-                <div className={styles["contact__title"]}>Name</div>
+              <div className={styles['me__contact']}>
+                <div className={styles['contact__title']}>Name</div>
                 <span>김용민</span>
               </div>
-              <div className={styles["me__contact"]}>
-                <div className={styles["contact__title"]}>Age</div>
-                <span>24</span>
+              <div className={styles['me__contact']}>
+                <div className={styles['contact__title']}>Age</div>
+                <span>25</span>
               </div>
-              <div className={styles["me__contact"]}>
-                <div className={styles["contact__title"]}>Address</div>
+              <div className={styles['me__contact']}>
+                <div className={styles['contact__title']}>Address</div>
                 <span>Siheung, Gyeonggi</span>
               </div>
-              <div className={styles["me__contact"]}>
-                <div className={styles["contact__title"]}>Phone</div>
+              <div className={styles['me__contact']}>
+                <div className={styles['contact__title']}>Phone</div>
                 <span>010 3703 0728</span>
               </div>
             </Col>
           </Row>
 
-          <div className="white-space-v" />
+          <div className='white-space-v' />
 
           <Row>
             <Col sm={7}>
-              <div className={styles["me__experience"]}>
-                <div className={styles["experience__title"]}>
+              <div className={styles['me__experience']}>
+                <div className={styles['experience__title']}>
                   My <span>Experience</span>
                 </div>
                 <Timeline
-                  period={"2021 - 2022"}
-                  agency={"세븐미어캣"}
-                  title={"풀스택 웹 개발"}
+                  period={'2022 - 2023'}
+                  agency={'SK 하이닉스'}
+                  title={'풀스택 웹 개발'}
+                >
+                  SK 하이닉스에서 상주 프리랜서로 프로젝트 참여.
+                  <br />
+                  예측모델 및 분석 대회 플랫폼 고도화.
+                  <br />
+                  회사에서 데이터와 해결과제를 등록하면, 사내 구성원들이 이를
+                  해결하는 모델을 개발하고 경쟁하는 플랫폼.
+                  <br />
+                  <b>#React</b> <b>#Spring Boot</b> <b>#Oracle</b>
+                </Timeline>
+
+                <Timeline
+                  period={'2022'}
+                  agency={'HYBE'}
+                  title={'프론트엔드 웹 개발'}
+                >
+                  HYBE에서 상주 프리랜서로 프로젝트 참여.
+                  <br />
+                  HYBE 및 HYBE 산하 레이블에서 사용할 웹 사이트 빌더 개발.
+                  <br />
+                  아임웹, Wix와 같은 서비스를 제공.
+                  <br />
+                  <b>#React</b>
+                </Timeline>
+
+                <Timeline
+                  period={'2021 - 2022'}
+                  agency={'세븐미어캣'}
+                  title={'풀스택 웹 개발'}
                 >
                   (주)세븐미어캣에서 풀스택 웹 개발자로 1년간 근무.
                   <br />
@@ -67,9 +95,9 @@ const Me = () => {
                 </Timeline>
 
                 <Timeline
-                  period={"2020 - 2021"}
-                  agency={"한국공학대"}
-                  title={"시각 장애인을 위한 학습 보조 점자 시스템"}
+                  period={'2020 - 2021'}
+                  agency={'한국공학대'}
+                  title={'시각 장애인을 위한 학습 보조 점자 시스템'}
                 >
                   사용자로부터 사진, 워드, ppt등의 파일을 수신하여 OCR 처리하는
                   서버 개발.
@@ -80,39 +108,12 @@ const Me = () => {
                 </Timeline>
               </div>
 
-              <div className={styles["me__certificate"]}>
-                <div className={styles["certificate__title"]}>Certificate</div>
+              <div className={styles['me__education']}>
+                <div className={styles['education__title']}>Education</div>
                 <Timeline
-                  period={"2021"}
-                  agency={"한국산업인력공단"}
-                  title={"정보처리기사"}
-                >
-                  정보처리기사 자격증 취득
-                </Timeline>
-
-                <Timeline
-                  period={"2020"}
-                  agency={"한국공학대"}
-                  title={"장려상"}
-                >
-                  2020 KPU 소프트웨어 경진대회
-                </Timeline>
-
-                <Timeline
-                  period={"2019"}
-                  agency={"한국공학대"}
-                  title={"장려상"}
-                >
-                  2019 KPU 소프트웨어 경진대회
-                </Timeline>
-              </div>
-
-              <div className={styles["me__education"]}>
-                <div className={styles["education__title"]}>Education</div>
-                <Timeline
-                  period={"2018 - 2022"}
-                  agency={"한국공학대"}
-                  title={"Computer Engineering"}
+                  period={'2018 - 2022'}
+                  agency={'한국공학대'}
+                  title={'Computer Engineering'}
                 >
                   한국공학대학교 컴퓨터공학과 - 수료
                 </Timeline>
@@ -120,17 +121,42 @@ const Me = () => {
             </Col>
 
             <Col sm={5}>
-              <div className={styles["me__tech-skill"]}>
-                <div className={styles["tech-skill__title"]}>
+              <div className={styles['me__certificate']}>
+                <div className={styles['certificate__title']}>Certificate</div>
+                <Timeline
+                  period={'2021'}
+                  agency={'한국산업인력공단'}
+                  title={'정보처리기사'}
+                >
+                  정보처리기사 자격증 취득
+                </Timeline>
+
+                <Timeline
+                  period={'2020'}
+                  agency={'한국공학대'}
+                  title={'장려상'}
+                >
+                  2020 KPU 소프트웨어 경진대회
+                </Timeline>
+
+                <Timeline
+                  period={'2019'}
+                  agency={'한국공학대'}
+                  title={'장려상'}
+                >
+                  2019 KPU 소프트웨어 경진대회
+                </Timeline>
+              </div>
+
+              <div className={styles['me__tech-skill']}>
+                <div className={styles['tech-skill__title']}>
                   Tech <span>Skills</span>
                 </div>
-                <Progress title={"React"} progress={90} />
-                <Progress title={"SCSS"} progress={85} />
-                <Progress title={"Node.js"} progress={90} />
-                <Progress title={"MySQL(MariaDB)"} progress={85} />
-                <Progress title={"AWS"} progress={80} />
-                <Progress title={"Nginx"} progress={80} />
-                <Progress title={"Git"} progress={90} />
+                <Progress title={'React'} progress={90} />
+                <Progress title={'Node.js'} progress={90} />
+                <Progress title={'Spring Boot'} progress={60} />
+                <Progress title={'MySQL(MariaDB)'} progress={85} />
+                <Progress title={'Oracle'} progress={85} />
               </div>
             </Col>
           </Row>
